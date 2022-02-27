c43e3181130f546d2f2431d982ba920287c23cd2
Ilya Muravyev <ilya.yarn@gmail.com>
2022-02-26 19:26:17 -0500

    Remove unused code

--- src/main/java/com/ilyamur/springcli/configuration/ApplicationConfiguration.java
+++ src/main/java/com/ilyamur/springcli/configuration/ApplicationConfiguration.java
@@ -1,18 +1,9 @@
 package com.ilyamur.springcli.configuration;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
-
-import java.nio.file.FileSystem;
-import java.nio.file.FileSystems;
 
 @Configuration
 @Profile(ApplicationProfile.PROD)
 public class ApplicationConfiguration {
-
-    @Bean
-    public FileSystem fileSystem() {
-        return FileSystems.getDefault();
-    }
 }
